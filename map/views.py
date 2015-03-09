0936816e441262239964e73fa8be2450690b92a1
zhengyusun <zhensn@gmail.com>
2015-03-08 19:13:52 -0700

    fixed map view and added template

--- map/views.py
+++ map/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 
 # Create your views here.
-def index(request):
-    return render_to_response("map/map.html", {}, context_instance=RequestContext(request))
+def map(request):
+    return render_to_response("map/map.html")
