2a17e568b01fd8cfd1950447ca5a4c94265d91f7
zhengyusun <zhensn@gmail.com>
2015-03-08 19:38:32 -0700

    added map

--- api/serializers.py
+++ api/serializers.py
@@ -5,8 +5,9 @@ from api.models import Message, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class MessageSerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
+    created = serializers.DateTimeField()
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    text = serializers.CharField(style={'base_template': 'textarea.html'})
+    text = serializers.CharField()
     lat = serializers.FloatField(min_value=-90, max_value=90)
     lng = serializers.FloatField(min_value=-180, max_value=180)
 
