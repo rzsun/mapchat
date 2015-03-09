6c792c8d76d9aa35256d4a253bcc98cf4e647e16
zhengyusun <zhensn@gmail.com>
2015-03-08 19:42:14 -0700

    fixed static files

--- mapchat/settings.py
+++ mapchat/settings.py
@@ -92,3 +92,6 @@ TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
